168521af23ee7d736ffd0de6136e8d7bc5919da9
Nox2004 <biel.bianchi2004@hotmail.com>
2024-12-11 17:08:42 -0300

    Agora vai

--- src/main.tsx
+++ src/main.tsx
@@ -1,3 +1,4 @@
+import BrowserRouter from 'react-router-dom/BrowserRouter'
 import { createRoot } from 'react-dom/client'
 import './styles/index.css'
 
@@ -8,9 +9,11 @@ import { I18nextProvider } from 'react-i18next'
 
 createRoot(document.getElementById('root')!).render(
   // <StrictMode>
+    <BrowserRouter basename={process.env.PUBLIC_URL}>
     <I18nextProvider i18n={i18next}>
       <App />
     </I18nextProvider>
+    </BrowserRouter>,
   // </StrictMode>,
   
 )
