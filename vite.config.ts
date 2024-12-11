451682e14d1932237cacff7fab9a5d5353e9bc7a
Nox2004 <biel.bianchi2004@hotmail.com>
2024-12-11 17:11:38 -0300

    aaaaaaaaaaaaaaa

--- vite.config.ts
+++ vite.config.ts
@@ -4,5 +4,5 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: '/Nox2004.github.io/'
+  base: '/Nox2004.github.io'
 })
